40726885c93e17417b0abf365f44524bf2123479
Joseph Francis <hookedupjoe@users.noreply.github.com>
2023-09-02 10:02:48 -0400

    reverted

--- appserver/actions/ws-main.js
+++ appserver/actions/ws-main.js
@@ -1,7 +1,6 @@
 'use strict';
 const THIS_MODULE_NAME = 'ws-main';
 const THIS_MODULE_TITLE = 'Send and receive Websock data related to this applications core features';
-const THIS_CODE_NAME = 'PhoneStreamer'
 
 var isSetup = false;
 var wssMain = false;
@@ -12,13 +11,13 @@ var users = {};
 module.exports.setup = function setup(scope,options) {
     var config = scope;
     var $ = config.locals.$;
+    // var OPEN_STATE = $.ws.WebSocket.OPEN;
 
     function Route() {
         this.name = THIS_MODULE_NAME;
         this.title = THIS_MODULE_TITLE;
     }
 
-    //--- Get the data we want to sent to the clients
     function getPeopleSummary(){
         var tmpList = {};
         for( var aID in users ){
@@ -36,25 +35,7 @@ module.exports.setup = function setup(scope,options) {
         wsRoom.sendDataToAll({action:'people', people: getPeopleSummary()});
     }
 
-    //--- Send a request to connect to the target client on behalf of the source client
-    function sendConnectRequest(theWS, theData){
-        var tmpName = '';
-        var tmpUserID = theWS.userid;
-        if( users[tmpUserID] ){
-            tmpName = users[tmpUserID].profile.name
-        }
-
-        if( users[theData.to] ){
-            var tmpUser = users[theData.to];
-            var tmpSocketID = tmpUser.socketid;
-            wsRoom.sendDataToClient(tmpSocketID, {action:'meetingrequest', offer: theData.offer, fromid: theWS.userid, fromname: tmpName, message: 'Meeting request from ' + tmpName})
-        } else {
-            wsRoom.sendDataToClient(theWS.id, {action:'meetingreply', fromid: theWS.userid, status: false, message: 'No longer available'})  
-        }
-    }
-    
-    //--- Send a connection response to the original source client on behalf of the target client
-    function sendConnectResponse(theWS, theData){
+    function sendMeetingResponse(theWS, theData){
         var tmpMsg = theData.message || {};
         
         var tmpName = '';
@@ -71,10 +52,27 @@ module.exports.setup = function setup(scope,options) {
         } else {
             console.log('unknown user',tmpMsg)
         }
+
+        
     }
     
-    //--- When a client creates or updates the user information, a new profile is sent back to the server
-    //     we refresh the details and send new people list to clients
+    function sendMeetingRequest(theWS, theData){
+        var tmpName = '';
+        var tmpUserID = theWS.userid;
+        if( users[tmpUserID] ){
+            tmpName = users[tmpUserID].profile.name
+        }
+
+        if( users[theData.to] ){
+            var tmpUser = users[theData.to];
+            var tmpSocketID = tmpUser.socketid;
+            wsRoom.sendDataToClient(tmpSocketID, {action:'meetingrequest', offer: theData.offer, fromid: theWS.userid, fromname: tmpName, message: 'Meeting request from ' + tmpName})
+        } else {
+            wsRoom.sendDataToClient(theWS.id, {action:'meetingreply', fromid: theWS.userid, status: false, message: 'No longer available'})  
+        }
+    }
+    
+    
     function updateProfile(theWS, theData){
         var tmpSocketID = theWS.id;
         var tmpUserID = theData.userid;
@@ -93,13 +91,11 @@ module.exports.setup = function setup(scope,options) {
         resendPeople();
     }
     
-    //--- When a new client connects, create a new unique ID and send a welcome message along with current people list
     function onConnect(ws){
         ws.userid = $.ws.mgr.getUniqueID();        
         ws.send(JSON.stringify({action: 'welcome', userid: ws.userid, id: ws.id, people:getPeopleSummary()}))
     }
 
-    //--- When a socket is removed, clear the user and resend the people list to clients
     function onMessage(ws,data,isBinary){
         var tmpData = (''+data).trim();
         if( tmpData.startsWith('{')){
@@ -109,9 +105,9 @@ module.exports.setup = function setup(scope,options) {
             if( tmpData.action == 'profile' && tmpData.profile){
                 updateProfile(ws,tmpData);
             } else if( tmpData.action == 'meeting'){
-                sendConnectRequest(ws,tmpData);
+                sendMeetingRequest(ws,tmpData);
             } else if( tmpData.action == 'meetingresponse'){
-                sendConnectResponse(ws,tmpData);
+                sendMeetingResponse(ws,tmpData);
                 
             } else {
                 console.log('unknown action',tmpData.action);
@@ -119,12 +115,10 @@ module.exports.setup = function setup(scope,options) {
         }
     }
 
-    //--- The client responds to the welcome message that handles the initial adding of a person / client
     function onSocketAdd(theID){
         //--- placeholder
     }
 
-    //--- When a socket is removed, clear the user and resend the people list to clients
     function onSocketRemove(theID){
         if( clients[theID] ){
             var tmpUserID = clients[theID].userid || '';
@@ -139,34 +133,20 @@ module.exports.setup = function setup(scope,options) {
         }
     }
 
-    //--- When called by the Winsock protocol, the below code handles the response
-    if( options && options.websocket === true ){
+    if( options.websocket === true ){
+
         if( !isSetup ){
-
-/**
- * Main Entrypoint
- * 
- * This creates a new node.js library WebSocketServer
- *  - noServer:true is used because we are fulfilling the websocket with an existing server enpoint
- *     ... instead of creating a new WebSocket server.
- *     ... (When using https, websockets must be wss, which is automatic in the client code)
- *  
- * This also creates a new WebSocketRoom, which is a simple control that tracks who comes and goes 
- *   and tell you about it. Also sends along messages to handle and a message when it connects
- * 
- * Note: A quick link to that code can be found in the readme of this repo.
- * 
- */
-
             wssMain = new $.ws.WebSocketServer({ noServer: true });
-            wsRoom = new $.ws.WebSocketRoom({name:THIS_CODE_NAME, server: wssMain, onConnect: onConnect, onMessage: onMessage, onSocketAdd: onSocketAdd, onSocketRemove: onSocketRemove, pingInterval:0 });
-            console.log(THIS_CODE_NAME + ' created new websock room')
+            wsRoom = new $.ws.WebSocketRoom({name:'stage', server: wssMain, onConnect: onConnect, onMessage: onMessage, onSocketAdd: onSocketAdd, onSocketRemove: onSocketRemove, pingInterval:0 });
+
+            isSetup = true;
+            console.log('Meeting Center created new websock room')
         }
+        
         return wssMain;
     }
 
-    //--- When called by the Winsock protocol, the below code does not run
-    //--- When called as a normal endpoint, the winsock code DOES NOT run but instead the below code runs
+    //--- For a Winsock Endpoint - This is never run.
     var base = Route.prototype;
     //==== End of common setup - add special stuff below
     //--- must have a "run" method *** 
@@ -203,7 +183,7 @@ module.exports.setup = function setup(scope,options) {
             var tmpResults = await (tmpRoute.run(req, res, next));
             res.json({
                 status: true,
-                people: getPeopleSummary()
+                results: tmpResults
             })
         } catch (ex) {
             res.json({ status: false, error: ex.toString() })
